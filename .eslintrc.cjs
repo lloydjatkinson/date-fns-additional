b2b3a02bef90ad7b79e6c71f23e4c9b90b4501c3
Lloyd Atkinson <lloydjatkinson@gmail.com>
2023-01-12 14:45:06 +0000

    build: initial setup eslint for linting and formatting - no specific standards applied other than initial sane options

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+    root: true,
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict',
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+        'tsconfigRootDir': __dirname,
+        'project': ['./tsconfig.json'],
+    },
+    'ignorePatterns': ['dist', 'docs'],
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
--- src/date-time-format.spec.ts
+++ src/date-time-format.spec.ts
@@ -2,4 +2,4 @@ import { describe, it, expect } from 'vitest';
 import type { FormatName, FormatPattern } from './date-time-format';
 import { formatUsingPattern, formatAsTwelveHourTime, formatAsTwelveHourTimeWithPeriod, formatAsTwentyFourHourTime, formatAsMonthNameWithDayNumber, formatAsMonthNameWithOrdinalDate, formatAsDateMonthYearAndTwelveHourTimeWithPeriod, formatAsIsoDate } from './date-time-format';
 
-describe('')
+describe('');
